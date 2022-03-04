a46237433792fc6cfe31aafea3c64d8d59bfe57f
git-since2005 <84366921+git-since2005@users.noreply.github.com>
2022-03-04 08:15:40 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,20 @@
 import streamlit as st
-def app():
+import pandas as pd
+
+def app(df):
 	st.title("Census visualisation web app")
 	st.write("This app allows a user to explore and visualise the census data")
+
+	if st.checkbox("Show column names of dataset"):
+		st.write(df.columns)
+	elif st.checkbox("Show columns data-type"):
+		col = st.selectbox('Select column', df.columns)
+		st.write(f'{col}')
+		st.write(f'Data-type:{df[col].dtype}')
+	elif st.checkbox("Show column summary"):
+		col1 = st.selectbox('Select column', df.describe().columns)
+		st.write(f'Name: {col1}')
+		st.write(f'Mean of {col1}: {df[col1].mean()}')
+		st.write(f'Median of {col1}: {df[col1].median()}')
+		st.write(f'Mode of {col1}: {df[col1].mode()}')
+		st.write(f'Standard deviation of {col1}: {df[col1].std()}')
